b0cfc143a74e6aeaf6f929fba467258006614774
Arjun S <nishkalankan1@gmail.com>
2021-07-02 00:21:58 +0530

    third commit

--- profile_module/lib/app.dart
+++ profile_module/lib/app.dart
@@ -17,159 +17,162 @@ class App extends StatelessWidget {
 }
 
 TextStyle titleStyle = new TextStyle(
-    fontSize: 29, letterSpacing: 0.5, fontWeight: FontWeight.w400);
-TextStyle subTitleStyle = new TextStyle(fontSize: 20, color: Colors.black54);
+    fontSize: 21, letterSpacing: 0.5, fontWeight: FontWeight.w400);
+TextStyle subTitleStyle = new TextStyle(fontSize: 15, color: Colors.black54);
 
 class StackWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.centerLeft,
-              end: Alignment.topRight,
-              colors: [
-                Colors.lightBlue,
-                Colors.lightBlue,
-                /*
-                Colors.white,
-                Colors.lightBlue,
-                Colors.lightBlue,
-                Colors.lightBlue,
-                Colors.lightBlue,
-                Colors.lightBlue,
-                Colors.lightBlue,
-                Colors.lightBlue,
-                Colors.blue,*/
-              ]),
-        ),
-        child: Column(children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(30, 30, 30, 10),
-            child: Material(
-              elevation: 10,
-              type: MaterialType.card,
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(30),
-              child: InkWell(
-                child: Container(
-                  height: MediaQuery.of(context).size.height / 2 - 70,
-                  width: MediaQuery.of(context).size.width - 10,
-                  margin: EdgeInsets.only(right: 10, left: 10, top: 10),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Center(
-                        child: Container(
-                            width: 190.0,
-                            height: 190.0,
-                            decoration: new BoxDecoration(
-                                shape: BoxShape.circle,
-                                image: new DecorationImage(
-                                    fit: BoxFit.fill,
-                                    image: new AssetImage(
-                                      "assets/GD.jpg",
-                                    )))),
-                      ),
-                      Container(
-                          child: Container(
-                              width: 200,
-                              height: 50,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(40.0),
-                                color: Colors.transparent.withOpacity(0.1),
-                              ),
-                              child: Center(
-                                  child: Text('Profile',
-                                      style: GoogleFonts.nunito(
-                                        textStyle: TextStyle(
-                                          letterSpacing: 2,
-                                          color: Colors.black87,
-                                          fontWeight: FontWeight.w700,
-                                          fontSize: 30,
-                                        ),
-                                      ))))),
-                    ],
-                  ),
+      color: Colors.white,
+      child: Column(children: <Widget>[
+        Padding(
+          padding: EdgeInsets.fromLTRB(30, 30, 30, 10),
+          child: Container(
+            height: MediaQuery.of(context).size.height / 4,
+            width: MediaQuery.of(context).size.width - 10,
+            margin: EdgeInsets.only(right: 10, left: 10, top: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Center(
+                  child: Container(
+                      width: 190.0 / 1.5,
+                      height: 190.0 / 1.5,
+                      decoration: new BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: new DecorationImage(
+                              fit: BoxFit.fill,
+                              image: new AssetImage(
+                                "assets/GD.jpg",
+                              )))),
                 ),
-                onTap: () {
-                  print("tapped on container");
-                },
-              ),
+                Container(
+                    child: Container(
+                        width: 200,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15.0),
+                          color: Colors.transparent.withOpacity(0.03),
+                        ),
+                        child: Center(
+                            child: Text('Profile',
+                                style: GoogleFonts.nunito(
+                                  textStyle: TextStyle(
+                                    letterSpacing: 2,
+                                    color: Color(0XFF6c757d),
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 30,
+                                  ),
+                                ))))),
+              ],
             ),
           ),
-          Container(
-            height: MediaQuery.of(context).size.height / 2,
-            width: MediaQuery.of(context).size.width,
-            margin: EdgeInsets.only(right: 30, left: 30, top: 10),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              /* gradient: LinearGradient(
-                  begin: Alignment.centerLeft,
-                  end: Alignment.topRight,
-                  colors: [
-                    Colors.white,
-                    Colors.lightBlue.shade100,
-                    Colors.lightBlue,
-                    Colors.blue,
-                  ]),*/
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            child: ListView(padding: EdgeInsets.only(left: 15),
-                /*crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,*/
-                children: <Widget>[
-                  SizedBox(height: 10),
-                  Text('Name',
-                      style: GoogleFonts.montserrat(textStyle: titleStyle)),
-                  Text('E. Gangadevi',
-                      style: GoogleFonts.montserrat(textStyle: subTitleStyle)),
-                  SizedBox(
-                    height: 20,
+        ),
+        Container(
+          height: MediaQuery.of(context).size.height / 2 + 90,
+          width: MediaQuery.of(context).size.width,
+          margin: EdgeInsets.only(right: 0, left: 0, top: 0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            /* gradient: LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.topRight,
+                      colors: [
+                        Colors.white,
+                        Colors.lightBlue.shade100,
+                        Colors.lightBlue,
+                        Colors.blue,
+                      ]),*/
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: ListView(
+              padding: EdgeInsets.only(left: 0),
+              physics: NeverScrollableScrollPhysics(),
+              children: <Widget>[
+                SizedBox(height: 10),
+                ListTile(
+                  tileColor: Colors.blue,
+                  leading: const Icon(
+                    Icons.person_rounded,
+                    size: 38,
                   ),
-                  Text('ID',
+                  title: Text('Name',
                       style: GoogleFonts.montserrat(textStyle: titleStyle)),
-                  Text('19-UCA-107',
+                  subtitle: Text('E. Gangadevi',
                       style: GoogleFonts.montserrat(textStyle: subTitleStyle)),
-                  SizedBox(
-                    height: 20,
+                ),
+                ListTile(
+                  tileColor: Colors.blue,
+                  leading: const Icon(
+                    Icons.card_membership,
+                    size: 38,
                   ),
-                  Text('D.O.B',
+                  title: Text('ID',
                       style: GoogleFonts.montserrat(textStyle: titleStyle)),
-                  Text('06/05/2001',
+                  subtitle: Text('19-UCA-107',
                       style: GoogleFonts.montserrat(textStyle: subTitleStyle)),
-                  SizedBox(
-                    height: 20,
+                ),
+                ListTile(
+                  tileColor: Colors.blue,
+                  leading: const Icon(
+                    Icons.date_range,
+                    size: 38,
                   ),
-                  Text('Gender',
+                  title: Text('D.O.B',
                       style: GoogleFonts.montserrat(textStyle: titleStyle)),
-                  Text('Female',
+                  subtitle: Text('06/05/2001',
                       style: GoogleFonts.montserrat(textStyle: subTitleStyle)),
-                  SizedBox(
-                    height: 20,
+                ),
+                ListTile(
+                  tileColor: Colors.blue,
+                  leading: const Icon(
+                    Icons.female,
+                    size: 38,
                   ),
-                  Text('Department',
+                  title: Text('Gender',
                       style: GoogleFonts.montserrat(textStyle: titleStyle)),
-                  Text('Computer Science',
+                  subtitle: Text('Female',
                       style: GoogleFonts.montserrat(textStyle: subTitleStyle)),
-                  SizedBox(
-                    height: 20,
+                ),
+                ListTile(
+                  tileColor: Colors.blue,
+                  leading: const Icon(
+                    Icons.cast_for_education,
+                    size: 38,
                   ),
-                  Text('Shift',
+                  title: Text('Department',
                       style: GoogleFonts.montserrat(textStyle: titleStyle)),
-                  Text('II',
+                  subtitle: Text('Computer Science',
                       style: GoogleFonts.montserrat(textStyle: subTitleStyle)),
-                  SizedBox(
-                    height: 20,
+                ),
+                ListTile(
+                  tileColor: Colors.blue,
+                  leading: const Icon(
+                    Icons.star_half,
+                    size: 38,
                   ),
-                  Text('E-mail ID',
+                  title: Text('Shift',
                       style: GoogleFonts.montserrat(textStyle: titleStyle)),
-                  Text('Chinhchingmangmang@gmail.com',
+                  subtitle: Text('II',
                       style: GoogleFonts.montserrat(textStyle: subTitleStyle)),
-                ]),
-          )
-        ]));
+                ),
+                ListTile(
+                  tileColor: Colors.blue,
+                  leading: const Icon(
+                    Icons.email_rounded,
+                    size: 38,
+                  ),
+                  title: Text('E-mail ID',
+                      style: GoogleFonts.montserrat(textStyle: titleStyle)),
+                  subtitle: Text('Chinhchingmangmang@gmail.com',
+                      style: GoogleFonts.montserrat(textStyle: subTitleStyle)),
+                ),
+              ]),
+        )
+      ]
+          //)//
+          ),
+    );
   }
 }
